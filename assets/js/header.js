40a756446e84671af058528e1ebd4c5c43b1546f
damianoimola <damiano.imola@outlook.it>
2021-01-13 21:18:39 +0100

    First commit of my webpage

---
+++ assets/js/header.js
@@ -0,0 +1,44 @@
+﻿// SCROLLING EFFECT
+$(window).scroll(function () {
+    let ypos = window.pageYOffset;
+
+    if (ypos > 0) {
+        let navbar = $(".navbar")
+
+        navbar.removeClass("transparent");
+        navbar.addClass("solid");
+    }
+    else {
+        let navbar = $(".navbar")
+
+        navbar.removeClass("solid");
+        navbar.addClass("transparent");
+    }
+});
+
+// OVERLAY MENU CLICKED
+$(document).ready(function () {
+
+    function switchMenuVisibility() {
+        let overlayMenu = $(".overlay-menu")
+
+        if (overlayMenu.hasClass("collapsed")) {
+
+            overlayMenu.removeClass("collapsed");
+            overlayMenu.addClass("expanded");
+
+        }
+        else if (overlayMenu.hasClass("expanded")) {
+
+            overlayMenu.removeClass("expanded");
+            overlayMenu.addClass("collapsed");
+
+        }
+    }
+
+    $(".hamburger").click(function () { switchMenuVisibility(); })
+    $(".times").click(function () { switchMenuVisibility(); })
+    $("#overlay-home").click(function () { switchMenuVisibility(); })
+    $("#overlay-about").click(function () { switchMenuVisibility(); })
+    $("#overlay-contacts").click(function () { switchMenuVisibility(); })
+})
