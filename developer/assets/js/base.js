7eac824efa90cc3f8764f130383a6ff115e8d236
damianoimola <damiano.imola@outlook.it>
2021-02-16 19:52:39 +0100

    small changes, added the date method

--- developer/assets/js/base.js
+++ developer/assets/js/base.js
@@ -1,3 +1,11 @@
-function getAge(){
+function computeAge(bornDate) {
+    let diff = Date.now() - bornDate.getTime();
+    let age_dt = new Date(diff); // full date-time
 
+    return Math.abs(age_dt.getUTCFullYear() + 1 - 1970); //TODO check if you can remove the "+1"
+}
+
+function main(){
+    // loading personal age
+    document.getElementById("age").innerText = computeAge(new Date(1999, 22, 3)).toString()
 }
