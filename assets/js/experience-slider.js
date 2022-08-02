93ecc3172b3ecee53ad8b1e49f181de8dd26af94
Damiano <damy_as_roma@live.it>
2022-08-03 00:55:06 +0200

    slider structure finished

--- assets/js/experience-slider.js
+++ assets/js/experience-slider.js
@@ -13,7 +13,8 @@ function loadExperienceData() {
             }
             return response.json();
         })
-        .then(data => loadExperiences(data));
+        //.then(data => loadExperiences(data));
+        .then(data => createSlider(data));
 }
 
 function loadExperiences(data){
@@ -77,6 +78,102 @@ function loadExperiences(data){
 
 
 
-function createSlider() {
+function createSlider(data) {
+    if(data === "" || data === undefined){
+        throw new TypeError("JSON not loaded!");
+    }
+
+    // let obj = JSON.parse(data);
+    let experience_description_wrapper = document.getElementById("experience-description-wrapper");
+    let experience_list_wrapper = document.getElementById("experience-list-wrapper");
+
+    let counter = 0;
+
+    data.Experience.forEach(item => {
+        let id = item.id;
+        let name = item.name;
+        let title = item.title;
+        let start = item.start;
+        let end = item.end;
+        let caption = item.caption_x;
+
+
+        let experience_description_header = document.createElement("div");
+        let experience_description = document.createElement("span");
+
+        if (counter === 1) {
+            experience_description_header.className = "experience-description-header visible";
+            experience_description.className = "experience-description visible";
+        } else {
+            experience_description_header.className = "experience-description-header";
+            experience_description.className = "experience-description";
+        }
+
+        experience_description.innerHTML = caption;
+
+
+
+
+        let list_name = document.createElement("p");
+        list_name.innerText = name;
+        list_name.onclick = function() {
+            makeExpVisible(id, item, data);
+        }
+        experience_list_wrapper.appendChild(list_name);
+
+
+        let company_name = document.createElement("div");
+        company_name.className = "company-name";
+        company_name.innerText = name;
+
+        let job_title = document.createElement("span");
+        job_title.className = "job-title";
+        job_title.innerText = title;
+
+        let date_start = document.createElement("span");
+        date_start.className = "date-start";
+        date_start.innerText = start;
+
+        let date_end = document.createElement("span");
+        date_end.className = "date-end";
+        date_end.innerText = end;
+
+
+        let leading = document.createElement("div");
+        leading.className = "leading";
+        leading.appendChild(company_name);
+        leading.appendChild(job_title);
+        let trailing = document.createElement("div");
+        trailing.className = "trailing";
+        trailing.appendChild(date_start);
+        trailing.appendChild(date_end);
+
+
+        experience_description_header.appendChild(leading);
+        experience_description_header.appendChild(trailing);
+
+
+
+        experience_description_wrapper.appendChild(experience_description_header);
+        experience_description_wrapper.appendChild(experience_description);
+
+        counter++;
+    });
+}
+
+
+function makeExpVisible(id, item, data) {
+    let experience_description_wrapper = document.getElementById("experience-description-wrapper");
+    let children = experience_description_wrapper.children;
+
+    for(let i=0; i<children.length/2; i++){
+        if(i === Number(id)) {
+            children[i * 2].classList.add("visible");
+            children[i * 2 + 1].classList.add("visible");
+        } else {
+            children[i * 2].classList.remove("visible");
+            children[i * 2 + 1].classList.remove("visible");
+        }
+    }
 
 }
